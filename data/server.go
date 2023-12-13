f2d0e691cf301bf05bc4ede7fdcf4bd593680510
david <davidjmacdonald11@gmail.com>
2023-12-13 09:40:51 -0500

    New init commit

--- data/server.go
+++
@@ -1,5 +0,0 @@
-package data
-
-type Server struct {
-    ID uint64 `gorm:"primaryKey"`
-}
--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"log"
-	"mcsm/data"
-	"mcsm/env"
-	"mcsm/routes"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/template/html/v2"
-)
+import "fmt"
 
 func main() {
-    data.MustConnectToDb()
-
-    app := fiber.New(fiber.Config{Views: html.New("./views", ".html") })
-    app.Static("/", "./public")
-
-    app.Use(routes.IsLoggedIn, routes.IsAdmin, cors.New(cors.Config {
-        AllowCredentials: true,
-        AllowOriginsFunc: func(origin string) bool {
-            return !env.IsProd()
-        },
-        AllowHeaders: "Content-Type, Origin, Accept",
-    }))
-
-    routes.Setup(app)
-
-    if !env.IsProd() {
-        log.Println("WARNING: This is not a production client!")
-        log.Println("Security is at risk!")
-    }
-
-    log.Fatal(app.Listen(env.Port()))
+    fmt.Println("Hello!")
 }
